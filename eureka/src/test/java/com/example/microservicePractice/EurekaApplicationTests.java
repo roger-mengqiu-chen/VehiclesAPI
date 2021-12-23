f37a94c56ff74b5e5d0786169aca34c690a47da2
Roger Chen <mengqiu.chen@hotmail.com>
2021-12-23 13:52:26 -0700

    Import Eureka module

---
+++ eureka/src/test/java/com/example/microservicePractice/EurekaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.microservicePractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
