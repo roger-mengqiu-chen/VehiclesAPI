a843c852f622eed07e4ee24d05d5ecc442b9bf99
Roger Chen <mengqiu.chen@hotmail.com>
2021-12-24 12:44:59 -0700

    Create test for PricingController

---
+++ pricing-service/src/test/java/com/udacity/pricing/api/PricingControllerTest.java
@@ -0,0 +1,35 @@
+package com.udacity.pricing.api;
+
+import com.udacity.pricing.service.PricingService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(PricingController.class)
+public class PricingControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    PricingService pricingService;
+
+    @Test
+    public void getPrice() throws Exception {
+        mockMvc.perform(get("/services/price?vehicleId=1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(jsonPath("$.currency").value("USD"))
+                .andExpect(jsonPath("$.price").exists());
+    }
+}
